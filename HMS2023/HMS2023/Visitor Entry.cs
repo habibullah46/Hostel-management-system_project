9530930598fcac9af3d066c600c8ef07d03024d5
Habibullah <103850736+habibullah46@users.noreply.github.com>
2023-10-14 02:30:34 +0500

    visitor entry

--- HMS2023/HMS2023/Visitor Entry.cs
+++ HMS2023/HMS2023/Visitor Entry.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using HMS2023.HelperClass;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +16,8 @@ namespace HMS2023
 {
     public partial class Visitor_Entry : Form
     {
+        helper hp = new helper();
+      
         public Visitor_Entry()
         {
             InitializeComponent();
@@ -19,7 +25,13 @@ namespace HMS2023
 
         private void btn_save_Click(object sender, EventArgs e)
         {
-           
+          //  string constring = ConfigurationManager.ConnectionStrings["myconstring"].ToString();
+           // string querry = "INSERT INTO tbl_Room VALUES('" + txt_Number.Text + "','" + txt_flour.Text + "','" + cmb_type.Text + "','" + cmb_AttachBAth.Text + "','" + txt_Rentseat.Text + "')";
+            string querry = "INSERT INTO tbl_VisitorEntry  VALUES('" + cmb_Visitor_cnic.Text+"','"+txt_Visitor_name.Text+"','"+txt_timedate.Text+"','"+txt_VRelation.Text+"','"+cmd_studentCNIC.Text+"','"+txt_Studentname.Text+"')";
+            hp.OpenCon();
+            hp.NonQuerryExecute(querry);
+            hp.CloseCon();
+            My_Message.success("Visitor Entry");
         }
     }
 }
