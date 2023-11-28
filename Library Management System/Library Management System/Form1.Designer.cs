a8946a3dfe800d8b99af4deab70278a12a598796
Habibullah <103850736+habibullah46@users.noreply.github.com>
2023-11-28 23:56:33 +0500

    login

--- Library Management System/Library Management System/Form1.Designer.cs
+++ Library Management System/Library Management System/Form1.Designer.cs
@@ -39,24 +39,24 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pbinsta = new System.Windows.Forms.PictureBox();
+            this.pbfacebook = new System.Windows.Forms.PictureBox();
+            this.pbtiwtter = new System.Windows.Forms.PictureBox();
             this.chkbox = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbinsta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbfacebook)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbtiwtter)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(153, 44);
+            this.pictureBox1.Location = new System.Drawing.Point(153, 38);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(154, 146);
+            this.pictureBox1.Size = new System.Drawing.Size(149, 152);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -136,6 +136,7 @@
             this.button1.TabIndex = 7;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -164,42 +165,45 @@
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // pictureBox4
+            // pbinsta
             // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(81, 502);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(70, 58);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 10;
-            this.pictureBox4.TabStop = false;
+            this.pbinsta.Image = ((System.Drawing.Image)(resources.GetObject("pbinsta.Image")));
+            this.pbinsta.Location = new System.Drawing.Point(109, 497);
+            this.pbinsta.Name = "pbinsta";
+            this.pbinsta.Size = new System.Drawing.Size(60, 45);
+            this.pbinsta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbinsta.TabIndex = 10;
+            this.pbinsta.TabStop = false;
+            this.pbinsta.Click += new System.EventHandler(this.pbinsta_Click);
             // 
-            // pictureBox5
+            // pbfacebook
             // 
-            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(167, 501);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(70, 58);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 11;
-            this.pictureBox5.TabStop = false;
+            this.pbfacebook.Image = ((System.Drawing.Image)(resources.GetObject("pbfacebook.Image")));
+            this.pbfacebook.Location = new System.Drawing.Point(175, 496);
+            this.pbfacebook.Name = "pbfacebook";
+            this.pbfacebook.Size = new System.Drawing.Size(60, 45);
+            this.pbfacebook.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbfacebook.TabIndex = 11;
+            this.pbfacebook.TabStop = false;
+            this.pbfacebook.Click += new System.EventHandler(this.pictureBox5_Click);
             // 
-            // pictureBox6
+            // pbtiwtter
             // 
-            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(256, 501);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(70, 58);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox6.TabIndex = 12;
-            this.pictureBox6.TabStop = false;
+            this.pbtiwtter.Image = ((System.Drawing.Image)(resources.GetObject("pbtiwtter.Image")));
+            this.pbtiwtter.Location = new System.Drawing.Point(242, 496);
+            this.pbtiwtter.Name = "pbtiwtter";
+            this.pbtiwtter.Size = new System.Drawing.Size(60, 45);
+            this.pbtiwtter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbtiwtter.TabIndex = 12;
+            this.pbtiwtter.TabStop = false;
+            this.pbtiwtter.Click += new System.EventHandler(this.pictureBox6_Click);
             // 
             // chkbox
             // 
             this.chkbox.AutoSize = true;
             this.chkbox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.chkbox.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkbox.Location = new System.Drawing.Point(287, 289);
+            this.chkbox.Location = new System.Drawing.Point(290, 294);
             this.chkbox.Name = "chkbox";
             this.chkbox.Size = new System.Drawing.Size(146, 24);
             this.chkbox.TabIndex = 13;
@@ -214,9 +218,9 @@
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(445, 617);
             this.Controls.Add(this.chkbox);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pbtiwtter);
+            this.Controls.Add(this.pbfacebook);
+            this.Controls.Add(this.pbinsta);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -234,9 +238,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbinsta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbfacebook)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbtiwtter)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -254,9 +258,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pbinsta;
+        private System.Windows.Forms.PictureBox pbfacebook;
+        private System.Windows.Forms.PictureBox pbtiwtter;
         private System.Windows.Forms.CheckBox chkbox;
     }
 }
--- Library Management System/Library Management System/Form1.cs
+++ Library Management System/Library Management System/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -55,6 +56,44 @@ namespace Library_Management_System
         private void txt_password_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://twitter.com/Habibul88592480/status/1721750203303133657?t=2WkrqMz8lymd9G9-nfMjhQ&s=19");
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("");
+        }
+
+        private void pbinsta_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://instagram.com/habib_khalqi?igshid=MmVlMjlkMTBhMg==");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string querry = "SELECT * FROM Login WHERE UserName='" + txt_username.Text + "' AND Password='"+txt_password.Text+"'";
+            string connection = System.Configuration.ConfigurationManager.ConnectionStrings["myconnecton"].ToString();
+
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(querry, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Dash_Board db = new Dash_Board();
+                db.Show(this);
+            }
+            else
+            {
+                MessageBox.Show("UserName or Password not Correct Try Again!");
+                txt_password.Clear();
+                txt_username.Clear();
+                txt_username.Focus();
+            }
         }
     }
 }
